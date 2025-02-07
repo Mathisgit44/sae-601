7be36407721bb6e420fb854b1ac2d38294177d3c
Mathisgit44 <boutin.e2303957@etud.univ-ubs.fr>
2025-02-07 20:43:23 +0100

    Update application.py

--- application.py
+++ application.py
@@ -13,7 +13,6 @@ run streamlit application.py
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import plotly.express as px
